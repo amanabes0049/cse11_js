cdb7492e01848bd62e0a5838aa4026a6287a256a
Aman Bhadwal <bhadwalaman610@gmail.com>
2026-02-26 23:37:57 +0530

    6

--- jsdatatype.js
+++ jsdatatype.js
@@ -1,4 +1,3 @@
-
 var num=10;
 console.log(num);
 console.log(typeof(num));
---
+++ loop.js
@@ -0,0 +1,7 @@
+var count;
+document.write("Starting the loop");
+for(count=0;count<10;count++){
+   document.write("Current count is: " + count);
+   document.write("<br>");
+}
+document.write("Loop ended");
