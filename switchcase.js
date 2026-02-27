179f50e2deef729f7994d986425e080a655dbbf0
Aman Bhadwal <bhadwalaman610@gmail.com>
2026-02-27 12:07:20 +0530

    #7

---
+++ switchcase.js
@@ -0,0 +1,20 @@
+var grade='A';
+switch(grade){
+    case 'A':
+        console.log("Excellent");
+        break;
+    case 'B':
+        console.log("Very Good");
+        break;
+    case 'C':
+        console.log("Average");
+        break;
+    case 'D':
+        console.log("Below Average");
+        break;
+    case 'F':
+        console.log("Failing");
+        break;
+    default:
+        console.log("Unknown grade");
+}
---
+++ whileloop.js
@@ -0,0 +1,8 @@
+var count = 0;
+document.write("Starting the loop");
+while(count < 10){
+    document.write("Current count is: " + count);
+    document.write("<br>");
+    count++;
+}
+document.write("Loop ended");
