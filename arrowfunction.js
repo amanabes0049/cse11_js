d05c400bfd4496e4b1e0f6ba41850066d99911e7
Aman Bhadwal <bhadwalaman610@gmail.com>
2026-03-17 10:33:48 +0530

    attributes and loops

--- arrowfunction.js
+++ arrowfunction.js
@@ -2,3 +2,5 @@ let add3=(a,b)=>{
   console.log(a+b);
 }
 add3(12,10);
+let add4=(a,b)=>a+b;
+console.log(add4(12,10));
--- loop.js
+++ loop.js
@@ -1,7 +1,16 @@
-var count;
-document.write("Starting the loop");
-for(count=0;count<10;count++){
-   document.write("Current count is: " + count);
-   document.write("<br>");
+//var count;
+//document.write("Starting the loop");
+//for(count=0;count<10;count++){
+   //document.write("Current count is: " + count);
+  // document.write("<br>");
+//}
+//document.write("Loop ended");
+let person={
+      name:"John",
+      age:30,
+      city:"New York"
+
 }
-document.write("Loop ended");
+for(let key in person){
+   console.log(key + ": " + person[key]);
+}
